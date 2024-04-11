108adfabad5b3584d150ee4cdefda51623641594
MarkMusili <musiliyrn@gmail.com>
2024-04-11 11:12:13 +0300

    Less than 5 lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,15 +12,7 @@ PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields, redaction, message, seperator) -> str:
-    """
-    ARGS:
-        fields: a list of strings representing all fields to obfuscate
-        redaction: a string representing by what the field will be obfuscated
-        message: a string representing the log line
-        separator: a string representing by which character
-            is separating all fields in the log line (message)
-    Returns the log message obsfuscated
-    """
+    """Returns the log message obsfuscated"""
     for field in fields:
         message = re.sub(rf'{field}=.*?{seperator}',
                          f'{field}={redaction}{seperator}', message)
