36b31dc0e54b2924e60326e4f9246260d28ba98e
MarkMusili <musiliyrn@gmail.com>
2024-04-11 11:10:41 +0300

    Pycodestyle check

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,13 +10,14 @@ from typing import List
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
-def filter_datum(fields, redaction, message, seperator):
+
+def filter_datum(fields, redaction, message, seperator) -> str:
     """
     ARGS:
         fields: a list of strings representing all fields to obfuscate
         redaction: a string representing by what the field will be obfuscated
         message: a string representing the log line
-        separator: a string representing by which character 
+        separator: a string representing by which character
             is separating all fields in the log line (message)
     Returns the log message obsfuscated
     """
