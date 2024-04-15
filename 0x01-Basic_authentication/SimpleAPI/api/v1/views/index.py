b819b75657b4eeb4715f9dd9cab45683dd16e6d2
MarkMusili <musiliyrn@gmail.com>
2024-04-15 11:00:39 +0300

    Syntax error fixed

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -26,8 +26,8 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', strict_slashes=False)
-def abort():
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized():
     """
     Abort the request
     """
