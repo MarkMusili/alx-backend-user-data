056042fe776f45c0381e3f17bde44a647f65362d
MarkMusili <musiliyrn@gmail.com>
2024-04-15 11:05:08 +0300

    Forbidden error message

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -32,3 +32,10 @@ def unauthorized():
     Abort the request
     """
     abort(401)
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden():
+    """
+    Abort the request
+    """
+    abort(403)
