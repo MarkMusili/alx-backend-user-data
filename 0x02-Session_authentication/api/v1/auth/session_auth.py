a354aadd9001ad321b34ec5d91bbc0bd4a73c825
MarkMusili <musiliyrn@gmail.com>
2024-04-17 15:27:22 +0300

    Create a session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,21 @@
 Session based authentication
 """
 from .auth import Auth
+from uuid import uuid4
 
 class SessionAuth(Auth):
     """
     Class implimentation for session authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a session id for user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
