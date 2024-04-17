0cbbfdc38d97ee1f1fa9899e1ee53a47988c76e6
MarkMusili <musiliyrn@gmail.com>
2024-04-17 15:15:54 +0300

    From Basic auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Session based authentication
+"""
+from .auth import Auth
+
+class SessionAuth(Auth):
+    """
+    Class implimentation for session authentication
+    """
+    pass
