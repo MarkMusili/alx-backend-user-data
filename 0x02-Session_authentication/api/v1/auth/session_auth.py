4f504b5c1e4ffe410b5b2fae815c73fb03765bd8
MarkMusili <musiliyrn@gmail.com>
2024-04-17 17:09:16 +0300

    Get a user based on their id from request

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Session based authentication
 """
 from .auth import Auth
+from models.user import User
 from uuid import uuid4
 
 class SessionAuth(Auth):
@@ -29,3 +30,11 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        Returns a user based on their cookie value
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
