b13b48f14d75705f09b9dfe50270ea7f3bfa7124
MarkMusili <musiliyrn@gmail.com>
2024-04-18 10:12:51 +0300

    Extra checks on the auth Basic

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -45,8 +45,9 @@ def before_request_func() -> None:
     # if auth.session_cookie(request) is None:
     #     abort(401)
 
-    # if not auth.authorization_header(request):
-    #     abort(401)
+    if not auth.authorization_header(request) \
+        and isinstance(auth, BasicAuth):
+        abort(401)
 
     # if auth.current_user(request) is None:
     #     abort(403)
