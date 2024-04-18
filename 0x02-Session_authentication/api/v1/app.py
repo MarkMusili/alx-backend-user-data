6ccb75df86c3a64ac507bd3526822e81680f80a3
MarkMusili <musiliyrn@gmail.com>
2024-04-18 10:45:14 +0300

    Removed extra checks on error 401

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -41,8 +41,7 @@ def before_request_func() -> None:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None \
-            and isinstance(auth, BasicAuth):
+    if auth.authorization_header(request) is None:
         abort(401)
 
     request.current_user = auth.current_user(request)
