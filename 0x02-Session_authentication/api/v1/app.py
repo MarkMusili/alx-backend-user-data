1555d213b311ec01cc6cee7b88c117d3cd0c05d1
MarkMusili <musiliyrn@gmail.com>
2024-04-18 09:59:15 +0300

    retracing steps task 0 checks

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -41,15 +41,15 @@ def before_request_func() -> None:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) and auth.session_cookie(request) is None:
-        abort(401)
-
-    if not auth.authorization_header(request):
-        abort(401)
-
     request.current_user = auth.current_user(request)
-    if auth.current_user(request) is None:
-        abort(403)
+    # if auth.session_cookie(request) is None:
+    #     abort(401)
+
+    # if not auth.authorization_header(request):
+    #     abort(401)
+
+    # if auth.current_user(request) is None:
+    #     abort(403)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
