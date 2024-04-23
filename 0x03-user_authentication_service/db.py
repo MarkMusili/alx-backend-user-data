0f42d499a811963bd4d94a174d8e790d965161f0
MarkMusili <musiliyrn@gmail.com>
2024-04-23 10:59:37 +0300

    Find filtered user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -38,3 +40,14 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwagrs):
+        """
+        Find a user in the database
+        """
+        try:
+            return self._session.query(User).filter_by(**kwagrs).one()
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
