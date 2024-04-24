48f8600949b03eac6c72f83da016fc16246e4837
MarkMusili <musiliyrn@gmail.com>
2024-04-24 15:06:07 +0300

    strict slashes param

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -32,7 +32,7 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'])
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
     """
     Login function
