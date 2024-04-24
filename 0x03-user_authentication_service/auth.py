2f294ae6e55b8ad2aea9f387e4ffbb2007026d2e
MarkMusili <musiliyrn@gmail.com>
2024-04-24 14:33:40 +0300

    Validate the loggin

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,3 +34,14 @@ class Auth:
             pass
         user = self._db.add_user(email, _hash_password(password))
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validate the login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode("utf-8"),
+                              user.hashed_password)
