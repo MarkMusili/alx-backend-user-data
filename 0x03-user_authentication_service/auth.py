1f7184e2f98868aa8d9d5d11d8198ca31f515744
MarkMusili <musiliyrn@gmail.com>
2024-04-24 17:41:19 +0300

    Get the user using their session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -65,3 +65,12 @@ class Auth:
             return session_id
         except Exception:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """
+        Get the user using their session_id
+        """
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except Exception:
+            return None
