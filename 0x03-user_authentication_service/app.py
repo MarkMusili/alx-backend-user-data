568bcccb8c345ee25bb789aa4a4ae0c0f9f658e6
MarkMusili <musiliyrn@gmail.com>
2024-04-24 15:10:55 +0300

    Use response object from jsonify

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -42,7 +42,7 @@ def login() -> str:
     if not auth.valid_login(email, password):
         abort(401)
     session_id = auth.create_session(email)
-    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
     return response
 
