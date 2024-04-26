2de8c06eecd454cde0abdd79e5588c16ac5bf23f
MarkMusili <musiliyrn@gmail.com>
2024-04-26 13:13:04 +0300

    Handling invalid session id

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -69,6 +69,8 @@ def profile():
     Get a user's profile
     """
     session_id = request.cookies.get('session_id')
+    if session_id is None:
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         return jsonify({"email": user.email}), 200
